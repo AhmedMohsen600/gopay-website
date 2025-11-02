61ca793abc9bd60d8f757cdcd236fe9180c5ecc2
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-02 16:39:26 +0200

    Add index.ts to export new icon components for payments and transfers

---
+++ app/[locale]/components/icons/index.ts
@@ -0,0 +1,17 @@
+import { PaidIcon } from "./paidIcon";
+import { UnpaidIcon } from "./unpaidIcon";
+import { DuePaymentsIcon } from "./duePaymentsIcons";
+import { TotalPaymentsIcon } from "./totalPaymentsIcons";
+import { TotalAccountsIcon } from "./totalAccounts";
+import { PaidRecurringIcon } from "./paidRecurringIcon";
+import { DailyTransferIcon } from "./dailyTransferIcon";
+
+export {
+  PaidIcon,
+  UnpaidIcon,
+  DuePaymentsIcon,
+  TotalPaymentsIcon,
+  TotalAccountsIcon,
+  PaidRecurringIcon,
+  DailyTransferIcon,
+};
