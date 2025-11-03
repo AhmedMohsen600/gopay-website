dd0b3c4e528b52acd3f3da6f50a44199ea51bb38
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-03 15:40:59 +0200

    Add PaymentChannelSection export to index for streamlined imports

--- app/[locale]/sections/index.ts
+++ app/[locale]/sections/index.ts
@@ -1,3 +1,4 @@
 // Export all sections from a single file for easier imports
 export { HeroSection } from "./HeroSection";
 export { PaymentStatsSection } from "./PaymentStatsSection";
+export { PaymentChannelSection } from "./PaymentChannelSection";
