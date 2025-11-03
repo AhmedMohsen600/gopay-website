fa1525e80e53cbb8f2be76cc1685ed00cfec1861
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-03 15:41:38 +0200

    style: Adjust header top margin for improved layout consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,10 +41,10 @@ export function Header() {
         initial={{ opacity: 0, y: -10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, y: { duration: 0.5 } }}
-        className="sticky top-4 mx-auto z-50 sm:w-full max-w-[1200px]"
+        className="sticky top-6 mx-auto  z-50 w-full max-w-[1200px]"
       >
         <nav
-          className="backdrop-blur-xl h-[66.4px] bg-white/24 lg:w-[1200px] rounded-xl px-6 py-3 flex items-center sm:justify-between gap-[10px]"
+          className="backdrop-blur-xl h-[66.4px] bg-white/24 lg:w-[1200px] lg:rounded-xl px-6 py-3 flex items-center sm:justify-between gap-[10px]"
           style={{ backdropFilter: "blur(20px)" }}
         >
           {/* Brand/Logo */}
