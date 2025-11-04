a60306c4bd41cfadb4b3574de0ad9063a54cbb89
shehab20089 <shehabmohsen1995@gmail.com>
2025-11-04 03:40:48 +0200

    feat(whoweare): Create WhoWeArePage to Integrate WhoWeAreHeroSection Component
    Added a new WhoWeArePage component that serves as a container for the WhoWeAreHeroSection, enhancing the structure and presentation of the "Who We Are" section in the application.

---
+++ app/[locale]/who-we-are/page.tsx
@@ -0,0 +1,9 @@
+import { WhoWeAreHeroSection } from "./components/who-we-are-hero-section";
+
+export default function WhoWeArePage() {
+  return (
+    <div>
+      <WhoWeAreHeroSection />
+    </div>
+  );
+}
