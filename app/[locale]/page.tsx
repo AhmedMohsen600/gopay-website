53f7b0595e3eba4e73e18954d06fd0970da0b363
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-04 15:43:10 +0200

    feat(home): add GoPayImpactShowcaseSection to the homepage
    Included the GoPayImpactShowcaseSection component in the homepage layout to enhance content presentation and user engagement. This addition aims to provide users with insights into GoPay's impact, complementing existing sections.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { HeroSection, PaymentChannelSection } from "./sections";
+import {
+  GoPayImpactShowcaseSection,
+  HeroSection,
+  PaymentChannelSection,
+} from "./sections";
 import { GopayProductSuiteSection } from "./sections/GopayProductSuiteSection";
 
 export default function Home() {
@@ -9,6 +13,7 @@ export default function Home() {
       <HeroSection />
       <PaymentChannelSection />
       <GopayProductSuiteSection />
+      <GoPayImpactShowcaseSection />
       {/** OTHER SECTIONS HERE SHOSHO */}
     </>
   );
