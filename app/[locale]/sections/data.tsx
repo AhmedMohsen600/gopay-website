dc250e77be5705f515c6f52952caf24aa21a2c95
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-05 19:09:17 +0200

    feat(data): add client logos array to showcase prominent clients in the application

--- app/[locale]/sections/data.tsx
+++ app/[locale]/sections/data.tsx
@@ -197,3 +197,94 @@ export const impactCardsData: ImpactCardData[] = [
     iconType: "down",
   },
 ];
+
+export const clientLogos = [
+  {
+    src: "https://framerusercontent.com/images/ZGXekz2SbwsanSSV6IAY9mtyc.png?scale-down-to=512&width=914&height=273",
+    alt: "Saudi Finance Company",
+  },
+  {
+    src: "https://framerusercontent.com/images/Lcj0LwR6PdG9wIwE3Uf9nSMwsQ.png?scale-down-to=512&width=862&height=312",
+    alt: "King Faisal School",
+  },
+  {
+    src: "https://framerusercontent.com/images/BZ5sJ2p6lBLEWvpBmjiLZj8Zd0.png?scale-down-to=512&width=1474&height=356",
+    alt: "Energy Tech",
+  },
+  {
+    src: "https://framerusercontent.com/images/L6hgPNJ3xpgF93Wkj9gBhLv0.png?scale-down-to=512&width=670&height=395",
+    alt: "Tabadul",
+  },
+  {
+    src: "https://framerusercontent.com/images/c5vOTMFCdUMkzbrtku3R4CPhvY.png?scale-down-to=512&width=1660&height=376",
+    alt: "Saudi Diesel Equipment",
+  },
+  {
+    src: "https://framerusercontent.com/images/1hxCdHcmn6V3cjWEZoQcDzfHc0.png?scale-down-to=512&width=1921&height=1081",
+    alt: "Kayan",
+  },
+  {
+    src: "https://framerusercontent.com/images/IZSGveDTbIDhCi9IvWmk77b9A.png?scale-down-to=512&width=1921&height=1081",
+    alt: "Royal Commission for AlUla",
+  },
+  {
+    src: "https://framerusercontent.com/images/6A6laD6Edx83nZn2IsH3998Eoc.png?scale-down-to=512&width=1920&height=1081",
+    alt: "Tawzea Aquapor",
+  },
+  {
+    src: "https://framerusercontent.com/images/D6AI4ZRysdaZIvEc2OZU5iTT4g.png?scale-down-to=512&width=662&height=223",
+    alt: "Gulf Health Council",
+  },
+  {
+    src: "https://framerusercontent.com/images/IGIej5nDpb5zc3tgvLN0KGzrvMg.png?width=400&height=400",
+    alt: "Obekan Digital Solutions",
+  },
+  {
+    src: "https://framerusercontent.com/images/0GUl24oECz88GQh6SEKqnIRe8e4.png?scale-down-to=512&width=722&height=308",
+    alt: "Hala Hala",
+  },
+  {
+    src: "https://framerusercontent.com/images/twewF3Px4rvl9nOwEwx4WSsstS4.png?scale-down-to=512&width=1920&height=1081",
+    alt: "Sayara",
+  },
+  {
+    src: "https://framerusercontent.com/images/Kob5cVGwXFIwlfdI7pmd1hOrDM.png?scale-down-to=512&width=1921&height=1081",
+    alt: "Dalah",
+  },
+  {
+    src: "https://framerusercontent.com/images/FlQunIZqjfebLdGMR3xDYucEE.png?width=400&height=221",
+    alt: "Four Winds Saudi Arabia",
+  },
+  {
+    src: "https://framerusercontent.com/images/IfNQS26Iqfi6OM4o9tYhcEyyxc.png?scale-down-to=512&width=620&height=403",
+    alt: "AgriServ",
+  },
+  {
+    src: "https://framerusercontent.com/images/RTsbG1m1XpyabN2HTGaLhSrrc.png?scale-down-to=512&width=1920&height=1080",
+    alt: "Riyad Bank",
+  },
+  {
+    src: "https://framerusercontent.com/images/6fh5BaVm8Ul4uSLjrGWV63v17g.png?scale-down-to=512&width=1920&height=1081",
+    alt: "Moyasar",
+  },
+  {
+    src: "https://framerusercontent.com/images/jwCpb73eFoHGox3cfH5ntFYaKA.png?scale-down-to=512&width=1633&height=888",
+    alt: "PayTabs",
+  },
+  {
+    src: "https://framerusercontent.com/images/1RSftnXeJUgSVSCTSJvrN2xPgjA.png?scale-down-to=512&width=600&height=81",
+    alt: "Foodics",
+  },
+  {
+    src: "https://framerusercontent.com/images/DgwN66giW57lciwQUWj8rqMYyFY.png?scale-down-to=512&width=1921&height=1081",
+    alt: "Tawteen",
+  },
+  {
+    src: "https://framerusercontent.com/images/9Gld5P9CS3xesOxMF80IDz8zc.png?width=500&height=250",
+    alt: "Urway Payment Gateway",
+  },
+  {
+    src: "https://framerusercontent.com/images/YkZIN6u1hLxgwivWa67bcC9VWlc.png?width=479&height=105",
+    alt: "Payments",
+  },
+];
