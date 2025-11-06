4b0c6a91f275e63ea3d800378ed100f35b1deaba
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-06 16:09:49 +0200

    feat(index): export GopayInNumbersSection to enhance component availability

--- app/[locale]/sections/index.ts
+++ app/[locale]/sections/index.ts
@@ -4,6 +4,7 @@ export { PaymentStatsSection } from "./PaymentStatsSection";
 export { PaymentChannelSection } from "./PaymentChannelSection";
 export { GopayProductSuiteSection } from "./GopayProductSuiteSection";
 export { GoPayImpactShowcaseSection } from "./GoPayImpactShowcaseSection";
+export { GopayInNumbersSection } from "./GopayInNumbersSection";
 export { OurClientsSection } from "./OurClientsSection";
 export { HowGopayWorksSection } from "./HowGopayWorksSection";
 export { WhyClientsSection } from "./WhyClientsSection";
