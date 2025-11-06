dcafa653539406cac62bc901edcb70ee7bbce729
shehab20089 <shehabmohsen1995@gmail.com>
2025-11-06 09:17:34 +0200

    feat(gopay-features-section): Enhance GoPayFeaturesSection with additional FeatureCards and responsive design
    Expanded the GoPayFeaturesSection to include multiple FeatureCards, each showcasing key features of GoPay with varying image variants and responsive layout adjustments. Improved overall styling for better user experience and visual consistency across different screen sizes.

--- app/[locale]/features/components/gopay-features-section.tsx
+++ app/[locale]/features/components/gopay-features-section.tsx
@@ -1,9 +1,61 @@
 import FeatureCard from "./feature-card/feature-card";
 export function GoPayFeaturesSection() {
   return (
-    <section className="max-w-[1440px] mx-auto px-10 pt-16 pb-[120px]">
-      <div className="grid grid-cols-2 max-w-[1000px] mx-auto">
-        <FeatureCard isFullRow imageVariant="dark-bg" />
+    <section className="max-w-[1440px] mx-auto xl:px-10 xl:pt-16 xl:pb-[120px] md:px-16 md:py-20 py-14 px-6 ">
+      <div className="grid md:grid-cols-2 grid-cols-1 max-w-[1000px] mx-auto gap-6">
+        <FeatureCard
+          isFullRow
+          imageVariant="dark-bg"
+          features={[
+            "Single integration to SADAD (national bill payment rail)",
+            "Automated payment references and real-time reconciliation",
+            "ERP & API-ready for full automation",
+            "SMS/email reminders to reduce overdue invoices",
+            "Full ZATCA compliance built-in",
+          ]}
+        />
+        <FeatureCard
+          imageVariant="light-1"
+          features={[
+            "Single integration to SADAD (national bill payment rail)",
+            "Automated payment references and real-time reconciliation",
+            "ERP & API-ready for full automation",
+            "SMS/email reminders to reduce overdue invoices",
+            "Full ZATCA compliance built-in",
+          ]}
+        />
+        <FeatureCard
+          imageVariant="light-2"
+          features={[
+            "Single integration to SADAD (national bill payment rail)",
+            "Automated payment references and real-time reconciliation",
+            "ERP & API-ready for full automation",
+            "SMS/email reminders to reduce overdue invoices",
+            "Full ZATCA compliance built-in",
+          ]}
+        />
+        <FeatureCard
+          imageVariant="light-3"
+          imageUrl="https://framerusercontent.com/images/dltRp4qIsFlPJXK0LAtdmFuymU.png?scale-down-to=1024&width=1060&height=394"
+          features={[
+            "Single integration to SADAD (national bill payment rail)",
+            "Automated payment references and real-time reconciliation",
+            "ERP & API-ready for full automation",
+            "SMS/email reminders to reduce overdue invoices",
+            "Full ZATCA compliance built-in",
+          ]}
+        />
+        <FeatureCard
+          imageVariant="light-4"
+          imageUrl="https://framerusercontent.com/images/pekNvaXoQZmZLXJYvEO1C7vjGU.png?width=548&height=836"
+          features={[
+            "Single integration to SADAD (national bill payment rail)",
+            "Automated payment references and real-time reconciliation",
+            "ERP & API-ready for full automation",
+            "SMS/email reminders to reduce overdue invoices",
+            "Full ZATCA compliance built-in",
+          ]}
+        />
       </div>
     </section>
   );
