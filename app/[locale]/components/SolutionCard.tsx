d0ace62ed263abedeb5994b03afd8ea405c4dd41
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-06 17:44:43 +0200

    fix(SolutionCard): adjust padding and icon size for improved responsiveness

--- app/[locale]/components/SolutionCard.tsx
+++ app/[locale]/components/SolutionCard.tsx
@@ -9,10 +9,10 @@ interface SolutionCardProps {
 
 export function SolutionCard({ icon: Icon, title }: SolutionCardProps) {
   return (
-    <div className="bg-[#f7f9fd] min-h-[150px]  rounded-xl border border-[#f5f4fb] p-4 flex gap-4">
+    <div className="bg-[#f7f9fd] min-h-auto md:min-h-[150px] rounded-xl border border-[#f5f4fb] p-3 md:p-4 flex gap-4">
       {/* Icon */}
       <div className="self-end">
-        <Icon className="w-8 h-8 text-secondary" />
+        <Icon className="md:w-8 md:h-8 w-[18px] h-[18px] text-secondary" />
 
         {/* Title */}
         <Typography variant="h6" className="text-text-5 mt-[13px]">
