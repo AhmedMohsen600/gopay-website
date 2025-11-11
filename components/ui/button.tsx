8339153a2275ed5db77c75f1f619fd5e60553857
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2025-11-11 16:16:21 +0200

    refactor(button): Adjust button styles for improved layout and responsiveness
    Modified button component styles to enhance padding and gap settings, ensuring better alignment and visual consistency across different screen sizes. Updated size definitions for the small variant to improve usability and aesthetics.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "group inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-link focus-visible:ring-link/50 focus-visible:ring-[3px]",
+  "group inline-flex items-center justify-center gap-1 py-[6px] md:py-2.5 md:px-3.5 px-2 py md:gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-link focus-visible:ring-link/50 focus-visible:ring-[3px]",
   {
     variants: {
       variant: {
@@ -19,7 +19,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-[42.4px] px-[14px] py-[10px] has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        sm: "h-[28.8px] rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
