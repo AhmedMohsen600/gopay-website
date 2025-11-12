b26ac9552b8276c1752aff736309ab3e903ca871
shehab20089 <shehabmohsen1995@gmail.com>
2025-11-12 14:01:43 +0200

    feat(blog): Add slug property to BlogItem interface and update blog data
    Enhanced the BlogItem interface by adding a slug property for better URL handling. Updated the blogsData array with new slug values for each blog entry, improving the structure and accessibility of blog links.

--- app/[locale]/blog/data.ts
+++ app/[locale]/blog/data.ts
@@ -1,4 +1,5 @@
 export interface BlogItem {
+  slug: string;
   imageSrc: string;
   imageAltKey: string;
   titleKey: string;
@@ -11,6 +12,7 @@ export interface BlogItem {
 
 export const blogsData: BlogItem[] = [
   {
+    slug: "media-coverage-of-gopay-booth-at-school-expo",
     imageSrc:
       "https://framerusercontent.com/images/pwOCQ86eV6uTDBY7fWUfJB09WU.jpg",
     imageAltKey: "blog.0.imageAlt",
@@ -19,6 +21,7 @@ export const blogsData: BlogItem[] = [
     dateKey: "blog.0.date",
   },
   {
+    slug: "gopay-participation-in-seamless-exhibition-1",
     imageSrc:
       "https://framerusercontent.com/images/hjAP4425jhk2cRYV1pkS6Z2y70.jpg",
     imageAltKey: "blog.1.imageAlt",
@@ -27,6 +30,7 @@ export const blogsData: BlogItem[] = [
     dateKey: "blog.1.date",
   },
   {
+    slug: "strategic-partnership-with-paytabs",
     imageSrc:
       "https://framerusercontent.com/images/9ce2k8cqOlGJfrJg6XNTd13Hg.jpg?scale-down-to=1024&width=1344&height=896",
     imageAltKey: "blog.2.imageAlt",
@@ -35,6 +39,7 @@ export const blogsData: BlogItem[] = [
     dateKey: "blog.2.date",
   },
   {
+    slug: "memorandum-of-understanding-with-alinma-bank",
     imageSrc:
       "https://framerusercontent.com/images/IHxbAFonDVAryRhTPcL2nK1d9g.png?width=1024&height=1024",
     imageAltKey: "blog.3.imageAlt",
@@ -43,6 +48,7 @@ export const blogsData: BlogItem[] = [
     dateKey: "blog.3.date",
   },
   {
+    slug: "memorandum-of-understanding-with-banque-saudi-fransi",
     imageSrc:
       "https://framerusercontent.com/images/oSm8gjptu9UhI2VAZWmFxtcsc.png?width=1856&height=2464",
     imageAltKey: "blog.4.imageAlt",
@@ -51,6 +57,7 @@ export const blogsData: BlogItem[] = [
     dateKey: "blog.4.date",
   },
   {
+    slug: "gopay-participation-in-seamless-exhibition-2",
     imageSrc:
       "https://framerusercontent.com/images/ovtVSKkCRIqiatRPO2CwFF3k88.png?scale-down-to=2048&width=1792&height=2688",
     imageAltKey: "blog.5.imageAlt",
